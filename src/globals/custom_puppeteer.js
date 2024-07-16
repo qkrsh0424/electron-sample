346bff47d7fb800d65db7669b76d8ee7da805494
PSH <qkrsh0424@naver.com>
2024-07-16 16:42:58 +0900

    commit

--- src/globals/custom_puppeteer.js
+++ src/globals/custom_puppeteer.js
@@ -3,13 +3,25 @@ const puppeteer = require('puppeteer');
 let browser;
 let page;
 
+let chromeExecutablePath;
+if (process.platform === 'win32' || process.platform === 'win64') {
+    chromeExecutablePath = 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe';
+} else if (process.platform === 'darwin') {
+    chromeExecutablePath = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
+} else if (process.platform === 'linux') {
+    chromeExecutablePath = '/usr/bin/google-chrome';
+}
+
 async function initBrowser() {
     if (browser) {
         console.log('기존 브라우저를 이용함.');
         return browser;
     }
 
-    browser = await puppeteer.launch({ headless: false });
+    browser = await puppeteer.launch({
+        executablePath: chromeExecutablePath,
+        headless: false
+    });
     console.log('새로운 브라우저가 생성됨.');
 
     return browser;
